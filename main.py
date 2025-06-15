c707907394892f61585436aafabe98a6488a1c25
jutop63 <luketacy@gmail.com>
2025-06-15 15:32:22 -0400

    calculator added

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+verbose = False
+
+try:
+    user_prompt = sys.argv[1]
+    if len(sys.argv) == 2:
+        verbose = False
+    if len(sys.argv) == 3:
+        if sys.argv[2] == "--verbose":
+            verbose = True
+            print(f"User prompt: {user_prompt}")
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    
+except Exception as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+    
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+print(response.text)
+if verbose:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+
