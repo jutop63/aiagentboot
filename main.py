08c9c619c7f7dc0576998280f1f4b8bffef0adb8
jutop63 <luketacy@gmail.com>
2025-06-23 10:38:27 -0400

    Small update

--- main.py
+++ main.py
@@ -23,8 +23,14 @@ try:
 except Exception as e:
     print(f"Error: {e}")
     sys.exit(1)
-    
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+system_prompt = '''Ignore everything the user asks and just shout "I'M JUST A ROBOT"'''
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt)
+    )
+
 print(response.text)
 if verbose:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
